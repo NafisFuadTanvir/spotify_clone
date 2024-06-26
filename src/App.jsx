a8b76c09d5d38c75807936c758a10a62aad49350
NafisFuadTanvir <nafisf026@gmail.com>
2024-06-26 21:57:34 +0600

    create the player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import Player from "./components/Player"
 import Sidebar from "./components/Sidebar"
 
 function App() {
  
 
   return (
-    <div className="h-screen bg-black">
+    <div className="h-screen bg-black ">
 
       <div className="h-[90%] flex">
         <Sidebar></Sidebar>
       </div>
+
+      
+      <Player></Player>
+      
      
     </div>
   )
